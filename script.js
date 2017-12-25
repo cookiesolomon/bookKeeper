9725b9b4c74bc9c7b30cee0e27a926db81664252
Cookie Solomon <shoshsolomon0@gmail.com>
2017-12-25 08:40:00 +0200

    changes 25.12.17

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ var submitForm = function () {
   // Add a rule so that the username must be over 5 characters. If the button is clicked and the username is too short then tell the user in a div below the button,
   // otherwise send an alert to thank them personally.
 
-
+  var isValid = true;
   var userName = document.getElementById("user-name").value;
   var errorMessage = document.getElementById("error");
 
@@ -29,11 +29,13 @@ var submitForm = function () {
   var passInput = document.getElementById("password").value;
   if (passInput.length >= 1 && passInput.length <= 6) {
     document.getElementById("pass-error").innerHTML = "Please enter a password that is more that 6 characters";
+    isValid = false;
   }
 
 
   if (userName.length >= 1 && userName.length < 5) {
     errorMessage.innerHTML = "The supplied username is too short try again using 5 or more characters";
+    isValid = false;
 
   } else if (userName.length >= 5) {
     alert("Thanks " + userName);
@@ -43,15 +45,17 @@ var submitForm = function () {
   var pass2 = document.getElementById("confirm_password").value;
   if (pass1 !== pass2) {
     document.getElementById("pass-error").innerHTML = "your passwords do not match";
+    isValid = false;
   }
 
   //Only allow the country to be either Israel or USA. If not you know what to do!
   var place = document.getElementById("countries").value;
-  
+
   //If the country is US or Israel things are good
   //If place !== Israel or place !== US and the quotes  things are bad
-  if(place !== "Israel" && place !== "United States of America"){
+  if (place !== "Israel" && place !== "United States of America") {
     document.getElementById("country-error").innerHTML = "invalid country";
+    isValid = false;
   }
 
 
@@ -62,16 +66,18 @@ var submitForm = function () {
   // }
 
   //Add code to validate that the phone number is shorter than 15 digits and if not..
-var phoneNum = document.getElementById("number").value;
-if(phoneNum.length > 0 && phoneNum.length < 15){
-  document.getElementById("phone-error").innerHTML = "please enter a valid phone number";
+  var phoneNum = document.getElementById("number").value;
+  if (phoneNum.length > 0 && phoneNum.length < 15) {
+    document.getElementById("phone-error").innerHTML = "please enter a valid phone number";
+    isValid = false;
 
-}
-else{
-  document.getElementById("validate").innerHTML = "Thanks for submitting the form correctly!";
-  
-  
-}
+  }
+  if (isValid) {
+
+    document.getElementById("validate").innerHTML = "Thanks for submitting the form correctly!";
+
+
+  }
 
 }
 
